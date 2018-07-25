fd626b67eca021bf7bd6e021016f7c4e0b0dd0ec
Geoff Corey <gwcorey@us.ibm.com>
2018-07-25 10:54:48 -0400

    update osx bootstrap

---
+++ bin/bootstrap-osx.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+brew install tmux fontconfig jq ncurses openssl python3 freetype gnupg neovim	pass vcsh go node wget git mr the_silver_searcher ctags
+pip3 install neovim
+nvim +PlugInstall
