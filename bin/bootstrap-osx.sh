f544b73a5096d609bca1bf1a6501da6f2bc541b1
Geoff Corey <gwcorey@us.ibm.com>
2019-05-22 14:39:20 -0400

    update bootstrap

--- bin/bootstrap-osx.sh
+++ bin/bootstrap-osx.sh
@@ -1,8 +1,15 @@
 #/bin/bash
 brew install tmux fontconfig jq ncurses openssl python3 freetype gnupg neovim	pass vcsh go node wget git mr the_silver_searcher ctags
 pip3 install neovim
-npm install -g fx vtop jsbeautify neovim loopback-cli eslint
+npm install -g fx vtop jsbeautify neovim loopback-cli eslint markdownlint npx
 nvim +PlugInstall
 curl https://install.meteor.com/ | sh
 brew tap cantino/mcfly https://github.com/cantino/mcfly
 brew install mcfly
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font
+brew install zsh-syntax-highlighting
+brew install zsh-autosuggestions
+git clone https://github.com/bhilburn/powerlevel9k.git ~/powerlevel9k
+mkdir -p ~/src/github.com/razee-io
+git clone https://github.com/razee-io/Kube-cloud-scripts.git ~/src/github.com/razee-io/
