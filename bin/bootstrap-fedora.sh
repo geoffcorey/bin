dd84742bec491f266af284a5456432135dd6e008
Geoff Corey <geoff@localhost.localdomain>
2021-01-30 12:36:38 -0500

    update fedora install

--- bin/bootstrap-fedora.sh
+++ bin/bootstrap-fedora.sh
@@ -1,29 +1,44 @@
 #!/usr/bin/env bash
 # Utilities, i3 wm and dev tools
 dnf -y groupinstall "C Development Tools and Libraries"
+dnf -y install dnf-plugins-core
+dnf -y copr enable heliocastro/hack-fonts
+dnf -y install   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf -y install   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf -y update
+dnf -y install lpf-spotify-client
 dnf -y install \
 	adobe-source-code-pro-fonts \
+	adobe-source-han-mono-fonts \
   acpi \
-  conky \
-  git \
-  gitflow \
   cmake \
   ctags \
+	docker-compose \
+	docker \
+  git \
+  gitflow \
+	ffmpeg \
+	fontawesome-fonts \
+  i3 \
+  i3lock \
   jq \
+	liveusb-creator \
+	motion \
   mr \
 	neovim \
   ngrep \
   openssl-devel \
-  python-devel \
+	openvpn \
+	procps \
 	python3-neovim \
   python-pip \
   ruby \
   sway \
-  i3 \
-  i3lock \
+	the_silver_searcher \
   tmux \
+  tree \
   weechat \
   vcsh \
   xbacklight \
   xdotool \
-  tree
+	youtube-dl
